519bb0f15dee6833bf34cb277b2ed2661a3c3b8a
Tanvirmehedi <tjmehedi201@gmail.com>
2022-08-24 03:47:53 +0600

    object

---
+++ chapterSeven/acseccingobject.js
@@ -0,0 +1,22 @@
+var obj = {
+  x: 10,
+  y: 20,
+  z: 30,
+};
+
+// console.log(obj.x);
+
+// console.log(obj.y);
+
+// console.log(obj.x + obj.z);
+
+// console.log(obj["x"]);
+
+// console.log(obj["y"]);
+
+// console.log(obj["x"] + obj["y"]);
+
+var show = "x";
+
+console.log(obj.show);
+console.log(obj[show]);
