0535437613d6af0ac3f4b153cda8eb043ac88a12
Tanvirmehedi <tjmehedi201@gmail.com>
2022-08-23 10:02:15 +0600

    chapterSeven

---
+++ chapterSeven/object.js
@@ -0,0 +1,25 @@
+var obj = {};
+
+obj.x = 90;
+
+// console.log(obj);
+
+var obj2 = {
+  x: 90,
+};
+// console.log(obj2);
+
+var point = {
+  x: 20,
+  y: 30,
+};
+
+point.z = 9;
+
+// console.log(point);
+
+var conObj = Object();
+
+conObj.a = 50;
+
+console.log(conObj);
